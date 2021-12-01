a2948afc2f20b7ef6ec917310be9e3d2ee8f15ce
maxime-forte <maxi-f@hotmail.fr>
2021-12-01 22:17:00 +0100

    intergation des models de production et conso

--- projet-c.deschamps/src/CSVbuilder/Reader.java
+++ projet-c.deschamps/src/CSVbuilder/Reader.java
@@ -85,24 +85,35 @@ public class Reader {
 
 
     static public Network read(String filename) throws IOException {
+
+        //initialisation des variables
 		FileReader in = new FileReader(filename);
 		BufferedReader bin = new BufferedReader(in);
         String lineselector;
 		Network network = new Network();
-        ConstantDevice consdevice = new ConstantDevice("mydevice",0,"const");
-        PeriodicDevice perdevice = new PeriodicDevice("mydevice",0,"periodic",0,0,0,0);
         City mycity = new City();
         int number =0;
         ArrayList<Device> listDevices = new ArrayList<Device>(0);
-        int nbDevice = 0;
+        int nbDevice = 0;        
+        Device consdevice = new ConstantDevice("mydevice",0,"const");
+        Device perdevice = new PeriodicDevice("mydevice",0,"periodic",0,0,0,0);
+        Device model1 = new Model1("model1",0,0,0);
+        Device model2 = new Model2(0,"model2",0,0,0,0);
+        Device model3 = new Model3(0,0,"model2",0,0,0);
+        Device model4 = new Model4(0,0,"model2",0,0,0,0);        
         int nbSys = 0;
         ArrayList<ProductionSystem> listSys = new ArrayList<ProductionSystem>(0);
         ProductionSystem constantsys = new ConstantSystem("constsys",0,"const");
         ProductionSystem periodicsys = new PeriodicSystem("periodicsys",0,"periodic",0,0,0,0);
-        //ArrayList<Link> listLinks = new ArrayList<>();
+        ProductionSystem eolian = new Eolian(0,0,0);
+        ProductionSystem dam = new Hydroeletric(0,0);
+        ProductionSystem fossil = new Fossil(0,0);
+        ProductionSystem nuclear = new Nuclear(0,0,0,0);
+        ProductionSystem solar = new Solar(0,0,0);
         Link mylink = new Link();
         double mylenght = 0;
         
+
 		while(bin.ready()) {
 			String line = bin.readLine();
 			String[] tokens = line.split(";");
@@ -110,20 +121,20 @@ public class Reader {
 
             switch(lineselector){
                 case "citystart":
-                    System.out.println("citystart");
+                    //DEGUG:System.out.println("citystart");
                     number++;
                     mycity = new City(0, false, Double.parseDouble(tokens[1].trim()), Double.parseDouble(tokens[2].trim()), number);
                     network.getListCities().add(mycity);
                 break;
                 
                 case "homestart":
-                    System.out.println("homestrat");
+                    //DEGUG:System.out.println("homestrat");
                     listDevices = new ArrayList<Device>(0);
                     nbDevice = 0;
                 break;
 
                 case "constantdevice":
-                System.out.println("consdev");
+                    //DEGUG:System.out.println("consdev");
                     consdevice = new ConstantDevice("mydevice",Double.parseDouble(tokens[1].trim()),"const");
                     listDevices.add(consdevice);
                     nbDevice++;
@@ -131,52 +142,105 @@ public class Reader {
                 break;
 
                 case "periodicdevice":
-                    System.out.println("perdiodicdev");
+                    //DEGUG:System.out.println("perdiodicdev");
                     perdevice = new PeriodicDevice("mydevice",Double.parseDouble(tokens[1].trim()),"periodic",Integer.parseInt(tokens[2].trim()),Integer.parseInt(tokens[3].trim()),Integer.parseInt(tokens[4].trim()),Integer.parseInt(tokens[5].trim()));
                     listDevices.add(perdevice);
                     nbDevice++;
                 break;
 
+                case "model1":
+                    model1 = new Model1("model1",Double.parseDouble(tokens[1].trim()),Double.parseDouble(tokens[2].trim()),Double.parseDouble(tokens[3].trim()));
+                    listDevices.add(model1);
+                    nbDevice++;
+                break;
+
+                case "model2":
+                    model2 = new Model2(Double.parseDouble(tokens[1].trim()),"model2",Double.parseDouble(tokens[2].trim()),Double.parseDouble(tokens[3].trim()),Integer.parseInt(tokens[4].trim()),Integer.parseInt(tokens[5].trim()));
+                    listDevices.add(model2);
+                    nbDevice++;
+                break;
+
+                case "model3":
+                    model3 = new Model3(Double.parseDouble(tokens[1].trim()),Double.parseDouble(tokens[2].trim()),"model3",Integer.parseInt(tokens[3].trim()),Integer.parseInt(tokens[4].trim()),Double.parseDouble(tokens[3].trim()));
+                    listDevices.add(model3);
+                    nbDevice++;
+                break;
+
+                case "model4":
+                    model4 = new Model4(Double.parseDouble(tokens[1].trim()),Double.parseDouble(tokens[2].trim()),"model4",Double.parseDouble(tokens[3].trim()),Double.parseDouble(tokens[4].trim()),Integer.parseInt(tokens[5].trim()),Double.parseDouble(tokens[3].trim()));
+                    listDevices.add(model4);
+                    nbDevice++;
+                break;
+
                 case "homeend":
-                    System.out.println("homeend");
+                    //DEGUG:System.out.println("homeend");
                     mycity.setNbHouses(mycity.getNbHouses()+1);
                     mycity.getCityCons().getListDelivery().add(new DeliveryPoint("foyer",nbDevice,listDevices));
                 break;
 
                 case "powerplantstart":
-                    System.out.println("pps");
+                    //DEGUG:System.out.println("pps");
                     listSys = new ArrayList<ProductionSystem>(0);
                     nbSys = 0;
                     mycity.setProducer(true);
                 break;
                 
                 case "constantsystem":
-                    System.out.println("cs");
+                    //DEGUG: System.out.println("cs");
                     constantsys = new ConstantSystem("constsys",Double.parseDouble(tokens[1].trim()),"const");
                     listSys.add(constantsys);
                     nbSys++;
                 break;
 
                 case "periodicsystem":
-                    System.out.println("ps");
+                    //DEGUG:System.out.println("ps");
                     periodicsys = new PeriodicSystem("periodicsys",Double.parseDouble(tokens[1].trim()),"periodic",Integer.parseInt(tokens[2].trim()),Integer.parseInt(tokens[3].trim()),Integer.parseInt(tokens[4].trim()),Integer.parseInt(tokens[5].trim()));
                     listSys.add(periodicsys);
                     nbSys++;
                 break;
 
+                case "eolian":
+                    eolian = new Eolian(Double.parseDouble(tokens[1].trim()),Double.parseDouble(tokens[2].trim()),Double.parseDouble(tokens[3].trim()));
+                    listSys.add(eolian);
+                    nbSys++;
+                break;
+
+                case "fossil":
+                    fossil = new Fossil(Double.parseDouble(tokens[1].trim()),Integer.parseInt(tokens[2].trim()));
+                    listSys.add(fossil);
+                    nbSys++;
+                break;
+
+                case "hydroeletric":
+                    dam = new Hydroeletric(Double.parseDouble(tokens[1].trim()),Double.parseDouble(tokens[2].trim()));
+                    listSys.add(dam);
+                    nbSys++;
+                break;
+
+                case "nuclear":
+                    nuclear = new Nuclear(Double.parseDouble(tokens[1].trim()),Double.parseDouble(tokens[2].trim()),Double.parseDouble(tokens[3].trim()),Integer.parseInt(tokens[4].trim()));
+                    listSys.add(nuclear);
+                    nbSys++;
+                break;
+
+                case "solar":
+                    solar = new Solar(Double.parseDouble(tokens[1].trim()),Double.parseDouble(tokens[2].trim()),Double.parseDouble(tokens[3].trim()));
+                    listSys.add(solar);
+                    nbSys++;
+                break;
 
                 case "powerplantend":
-                    System.out.println("ppe");
+                    //DEGUG:System.out.println("ppe");
                     mycity.getCityProd().getListInj().add(new InjectionPoint("powerplant",nbSys,listSys));
                 break;
 
                 case "cityend":
-                    System.out.println("ce");
+                    //DEGUG:System.out.println("ce");
                     network.getListCities().add(mycity);
                 break;
 
                 case "link":
-                    System.out.println("l");
+                    //DEGUG:System.out.println("l");
                     City cityToLink1 = network.getCityInList(Integer.parseInt(tokens[1].trim()), network.getListCities());
                     City cityToLink2 = network.getCityInList(Integer.parseInt(tokens[2].trim()), network.getListCities());
                     mylenght = network.calculateLength(cityToLink1,cityToLink2);
