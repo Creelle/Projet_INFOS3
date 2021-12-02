1131940e6f1b915970a4c18485ae5d4cc2365d3b
maxime-forte <maxi-f@hotmail.fr>
2021-12-03 00:35:07 +0100

    adapter csv pour ^tre réaliste

--- projet-c.deschamps/src/CSVbuilder/ReaderMain.java
+++ projet-c.deschamps/src/CSVbuilder/ReaderMain.java
@@ -5,8 +5,8 @@ import Network.*;
 public class ReaderMain {
     
     public static void main(String[] args) throws IOException {
-        Network network = Reader.read("data/net.csv");
-        System.out.println(network.getCityInList(3, network.getListCities()).getCityProd().getListInj().get(0).getListSys().get(0).getPowerMax());
+        Network network = Reader.read("data/meganetwork.csv");
+        //System.out.println(network.getCityInList(3, network.getListCities()).getCityProd().getListInj().get(0).getListSys().get(0).getPowerMax());
         network.displayListCities();
         network.displayListLinks();
         network.plotGraphNetwork();
