57bbba62980440f3a7b05898f706c4bfc7372bf1
ceddesch <ced.desch@gmail.com>
2021-12-03 22:19:40 +0100

    maj3

--- projet-c.deschamps/src/IntegratedExtensions/SimulateAllExtensionsMain.java
+++ projet-c.deschamps/src/IntegratedExtensions/SimulateAllExtensionsMain.java
@@ -3,12 +3,16 @@ package IntegratedExtensions;
 import java.io.IOException;
 
 import Network.*;
+import CSVbuilder.*;
 public class SimulateAllExtensionsMain {
     public static void main(String[] args) throws IOException {
-        Network network = new Network(6);
+        Network network = Reader.read("data/meganetwork.csv");
+        System.out.println(network.getCityInList(3, network.getListCities()).getCityProd().getListInj().get(0).getListSys().get(0).getName());
+        System.out.println(network.getCityInList(1, network.getListCities()).getCityProd().getListInj().get(0).getListSys().get(0).getName());
         network.displayListCities();
         network.displayListLinks();
         network.plotGraphNetwork();
-        network.simulation(1, true);
+        network.simulation(1, true);        
+        network.simulateOnAYear();
     }
 }
