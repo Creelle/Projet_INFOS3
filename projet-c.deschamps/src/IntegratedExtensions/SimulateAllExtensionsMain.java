f446bbd6223f4c730e1a745429072cb397b81983
maxime-forte <maxi-f@hotmail.fr>
2021-12-03 22:27:55 +0100

    Update SimulateAllExtensionsMain.java

--- projet-c.deschamps/src/IntegratedExtensions/SimulateAllExtensionsMain.java
+++ projet-c.deschamps/src/IntegratedExtensions/SimulateAllExtensionsMain.java
@@ -7,8 +7,6 @@ import CSVbuilder.*;
 public class SimulateAllExtensionsMain {
     public static void main(String[] args) throws IOException {
         Network network = Reader.read("data/meganetwork.csv");
-        System.out.println(network.getCityInList(3, network.getListCities()).getCityProd().getListInj().get(0).getListSys().get(0).getName());
-        System.out.println(network.getCityInList(1, network.getListCities()).getCityProd().getListInj().get(0).getListSys().get(0).getName());
         network.displayListCities();
         network.displayListLinks();
         network.plotGraphNetwork();
