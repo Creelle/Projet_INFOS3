7582a77ec4fb155fcdc66a20baa5cb588908ed48
maxime-forte <maxi-f@hotmail.fr>
2021-12-04 15:46:13 +0100

    Update ReaderMain.java

--- projet-c.deschamps/src/CSVbuilder/ReaderMain.java
+++ projet-c.deschamps/src/CSVbuilder/ReaderMain.java
@@ -5,7 +5,7 @@ import Network.*;
 public class ReaderMain {
     
     public static void main(String[] args) throws IOException {
-        Network network = Reader.read("data/TO_LOAD/meganetwork.csv");
+        Network network = Reader.read("data/TO_LOAD/network2.csv");
         network.displayListCities();
         network.displayListLinks();
         network.plotGraphNetwork();
