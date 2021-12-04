0b40e3e1326f780cf0a1f0ff165d57e397524415
ceddesch <ced.desch@gmail.com>
2021-12-04 17:10:35 +0100

    maj

--- projet-c.deschamps/src/Network/Link.java
+++ projet-c.deschamps/src/Network/Link.java
@@ -5,7 +5,6 @@ public class Link {
     
     //Attributs
     private double linkLength; //Nombre de villes dans le Réseau (en km)
-    //private double[] transportedPower; //Puissance transportée par le lien au départ (W)
     private double lineicLoss; //Perte linéique de puissance (W/km)
     private int start; //Départ du lien
     private int end; //Fin du lien
@@ -47,14 +46,6 @@ public class Link {
         this.linkLength = linkLength;
     }
 
-    /*public double[] getTransportedPower() {
-        return this.transportedPower;
-    }
-
-    public void setTransportedPower(double[] transportedPower) {
-        this.transportedPower = transportedPower;
-    }*/
-
     public double getLineicLoss() {
         return this.lineicLoss;
     }
@@ -78,44 +69,5 @@ public class Link {
     public void setEnd(int end) {
         this.end = end;
     }
-
-    //Méthodes
-
-    /**
-     * Méthode injectant le surplus de production d'une autre ville dans une certaine ville
-     * 
-     * @param prod tableau de production existant de la ville considérée
-     */
-    /*public void inject(double[] prod){
-        for(int k = 0; k<1440; k++){
-            prod[k]+= transportedPower[k]-lineicLoss*linkLength;
-        }
-    }*/
-
-    
-    /**
-     * Génère le tableau de puissance en trop dans la ville proportionnellement au nombre
-     * de ville ne produisant pas
-     * @param city
-     * @param j
-     * @param ratio inverse du nombre de villes ne produisant pas
-     *  à laquelle la ville considérée est reliée
-     * @return le tableau de puissance en trop
-     */
-    /*public double[] takeSurplus(City city, ArrayList<double[]> listTableProd, ArrayList<double[]> listTableCons, double ratio){
-        double[] powerToTransport = new double[1440];
-        double[] prod = listTableProd.get(city.getNumber()-1);
-        double[] cons = listTableCons.get(city.getNumber()-1);
-        for(int k = 0; k<1440; k++){
-            double surplus = Math.round((prod[k] - cons[k])/ratio*10.0)/10.0;
-            if(surplus > 0){
-                powerToTransport[k] = surplus;
-                prod[k] = prod[k] - surplus; //L'énergie n'est plus dispo dans cette ville!
-            }else{
-                powerToTransport[k] = 0;
-            }
-        }
-        return powerToTransport;
-    }*/
 
 }
